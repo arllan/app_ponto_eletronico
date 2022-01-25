f20c8a97bc8ba7591fdda81eb436106bac32d520
Arllan Pablo <arllanpablo@gmail.com>
2022-01-24 21:06:11 -0300

    feat: initial commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View></View>
+    </SafeAreaView>
+  );
+}
